979b8e932de0110f71cdbeaec61d5e6841b843e4
niksaz <nikitasazanovich@gmail.com>
2020-12-10 16:56:51 +0300

    Reproduction of all offline experiments for nipsA (except GBDT).

---
+++ experiments/run_offline_experiments.sh
@@ -0,0 +1,11 @@
+bash offline_scripts/nipsA_lr_offline.sh > results/offline_scripts/nipsA_lr_offline.sh
+bash offline_scripts/nipsA_fm_offline.sh > results/offline_scripts/nipsA_fm_offline.sh
+bash offline_scripts/nipsA_wideNdeep_offline.sh > results/offline_scripts/nipsA_wideNdeep_offline.sh
+
+bash offline_scripts/nipsA_deepfm_offline.sh > results/offline_scripts/nipsA_deepfm_offline.sh
+bash offline_scripts/nipsA_pnn_offline.sh > results/offline_scripts/nipsA_pnn_offline.sh
+bash offline_scripts/nipsA_gbdt_offline.sh > results/offline_scripts/nipsA_gbdt_offline.sh
+
+bash offline_scripts/nipsA_d1_offline.sh > results/offline_scripts/nipsA_d1_offline.sh
+bash offline_scripts/nipsA_gbdt2nn_offline.sh > results/offline_scripts/nipsA_gbdt2nn_offline.sh
+bash offline_scripts/nipsA_deepgbm_offline.sh > results/offline_scripts/nipsA_deepgbm_offline.sh
